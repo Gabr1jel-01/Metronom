a2c034296942122105234dede5c6fcffa040c13c
Gabrijel <g.gordic@outlook.com>
2023-02-18 22:24:58 +0100

    Create metronom.py

---
+++ metronom.py
@@ -0,0 +1,35 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix import label 
+
+
+
+
+
+class TestApp(App):
+
+    def build(self):
+        # return a Button() as a root widget
+        return Button(text='hello world', font_size= 70)
+
+
+
+
+
+This 
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    TestApp().run()

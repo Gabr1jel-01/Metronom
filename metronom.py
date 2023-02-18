dd7196e6c0a64051d34477b7cedc15f245462271
Gabrijel <g.gordic@outlook.com>
2023-02-18 22:28:11 +0100

    Update metronom.py

--- metronom.py
+++ metronom.py
@@ -2,33 +2,14 @@ import kivy
 
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix import label 
-
-
-
+from kivy.uix import label
 
 
 class TestApp(App):
 
     def build(self):
         # return a Button() as a root widget
-        return Button(text='hello world', font_size= 70)
-
-
-
-
-
-This 
-
-
-
-
-
-
-
-
-
-
+        return Button(text='hello world', font_size=70)
 
 
 if __name__ == '__main__':
